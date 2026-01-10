498b29713a110c607fc20921b03eb5649d4c3e55
ハイドラ <thecomputer314@gmail.com>
2026-01-10 17:38:46 -0600

    Format

--- main.py
+++ main.py
@@ -16,24 +16,31 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy.linalg import norm
-from sleipnir.autodiff import VariableMatrix
-from sleipnir.optimization import Problem, ExitStatus
-from sleipnir.autodiff import atan2
-from sleipnir.autodiff import hypot
+from sleipnir.autodiff import VariableMatrix, atan2, hypot
+from sleipnir.optimization import ExitStatus, Problem
 
-field_width = 8.043 # m
-field_length = 16.518 # m
+field_width = 8.043  # m
+field_length = 16.518  # m
 target_wrt_field = np.array(
-    [[(158.6+(47/2))*.0254], [field_width / 2.0], [72 * .0254], [0.0], [0.0], [0.0]]
+    [
+        [(158.6 + (47 / 2)) * 0.0254],
+        [field_width / 2.0],
+        [72 * 0.0254],
+        [0.0],
+        [0.0],
+        [0.0],
+    ]
 )
 target_radius = 0.5  # m
 g = 9.81  # m/s²
 max_shooter_velocity = 15  # m/s
-ball_mass = .5 / 2.205 # kg
-ball_diameter = 5.91 * .0254 # m
+ball_mass = 0.5 / 2.205  # kg
+ball_diameter = 5.91 * 0.0254  # m
+
 
 def lerp(a, b, t):
     return a + t * (b - a)
+
 
 def f(x):
     # x' = x'
@@ -58,13 +65,14 @@ def f(x):
         [[v_x], [v_y], [v_z], [-a_D(v_x)], [-a_D(v_y)], [-g - a_D(v_z)]]
     )
 
+
 def min_velocity(robot_x, robot_y, robot_vx, robot_vy):
     # Robot initial state
     robot_wrt_field = np.array(
         [[robot_x], [robot_y], [0.0], [robot_vx], [robot_vy], [0.0]]
     )
 
-    shooter_wrt_robot = np.array([[0.0], [0.0], [20 * .0254], [0.0], [0.0], [0.0]])
+    shooter_wrt_robot = np.array([[0.0], [0.0], [20 * 0.0254], [0.0], [0.0], [0.0]])
     shooter_wrt_field = robot_wrt_field + shooter_wrt_robot
 
     problem = Problem()
@@ -136,7 +144,7 @@ def min_velocity(robot_x, robot_y, robot_vx, robot_vy):
     # Minimize initial velocity
     problem.minimize(v0_wrt_shooter.T @ v0_wrt_shooter)
 
-    if problem.solve(tolerance = .01) == ExitStatus.SUCCESS:
+    if problem.solve(tolerance=0.01) == ExitStatus.SUCCESS:
         print("Minimum velocity solve:")
         # Initial velocity vector with respect to shooter
         v0 = v0_wrt_shooter.value()
@@ -152,9 +160,10 @@ def min_velocity(robot_x, robot_y, robot_vx, robot_vy):
 
         print(f"Total time = {T.value():.03f} s")
 
-        return True,velocity, pitch, yaw, X
+        return True, velocity, pitch, yaw, X
     print("Infeasible at this location")
-    return False,0
+    return False, 0
+
 
 def fixed_velocity(robot_x, robot_y, robot_vx, robot_vy, target_vel, prev_X):
     # Robot initial state
@@ -162,7 +171,7 @@ def fixed_velocity(robot_x, robot_y, robot_vx, robot_vy, target_vel, prev_X):
         [[robot_x], [robot_y], [0.0], [robot_vx], [robot_vy], [0.0]]
     )
 
-    shooter_wrt_robot = np.array([[0.0], [0.0], [20 * .0254], [0.0], [0.0], [0.0]])
+    shooter_wrt_robot = np.array([[0.0], [0.0], [20 * 0.0254], [0.0], [0.0], [0.0]])
     shooter_wrt_field = robot_wrt_field + shooter_wrt_robot
 
     problem = Problem()
@@ -210,9 +219,7 @@ def fixed_velocity(robot_x, robot_y, robot_vx, robot_vy, target_vel, prev_X):
 
     # Velocity initial guess is last solve's velocity
     for k in range(N):
-        v[:, k].set_value(
-            prev_v[:, k].value()
-        )
+        v[:, k].set_value(prev_v[:, k].value())
 
     # Shooter initial position
     problem.subject_to(p[:, :1] == shooter_wrt_field[:3, :])
@@ -225,12 +232,14 @@ def fixed_velocity(robot_x, robot_y, robot_vx, robot_vy, target_vel, prev_X):
         (v_x[0] - robot_wrt_field[3, 0]) ** 2
         + (v_y[0] - robot_wrt_field[4, 0]) ** 2
         + (v_z[0] - robot_wrt_field[5, 0]) ** 2
-        == target_vel ** 2
+        == target_vel**2
     )
 
     # Require initial velocity pitch to be higher than the previous pitch
-    problem.subject_to(atan2(v_z[0], hypot(v_x[0], v_y[0])) > atan2(prev_v_z[0], hypot(prev_v_x[0],
-                                                                                      prev_v_y[0])))
+    problem.subject_to(
+        atan2(v_z[0], hypot(v_x[0], v_y[0]))
+        > atan2(prev_v_z[0], hypot(prev_v_x[0], prev_v_y[0]))
+    )
 
     # Dynamics constraints - RK4 integration
     h = dt
@@ -250,7 +259,7 @@ def fixed_velocity(robot_x, robot_y, robot_vx, robot_vy, target_vel, prev_X):
     # Require the final velocity is at least 20 degrees downwards
     problem.subject_to(atan2(v_z[-1], hypot(v_x[-1], v_y[-1])) < (-20 * math.pi / 180))
 
-    if problem.solve(tolerance = .01) == ExitStatus.SUCCESS:
+    if problem.solve(tolerance=0.01) == ExitStatus.SUCCESS:
         print("Fixed velocity solve:")
         # Initial velocity vector with respect to shooter
         v0 = v0_wrt_shooter.value()
@@ -266,9 +275,10 @@ def fixed_velocity(robot_x, robot_y, robot_vx, robot_vy, target_vel, prev_X):
 
         print(f"Total time = {T.value():.03f} s")
 
-        return True,velocity,pitch,yaw,X
+        return True, velocity, pitch, yaw, X
     print(f"Infeasible at this location with velocity {target_vel:.03f} m/s")
-    return False,0
+    return False, 0
+
 
 if __name__ == "__main__":
     x = 0.3
@@ -279,22 +289,22 @@ if __name__ == "__main__":
     velocities = []
     angles = []
 
-    min_vel_solve = min_velocity(x,y,vx,vy)
+    min_vel_solve = min_velocity(x, y, vx, vy)
     if min_vel_solve[0]:
         velocities.append(min_vel_solve[1])
         angles.append(np.rad2deg(min_vel_solve[2]))
-        d_V = .1
+        d_V = 0.1
         vel = min_vel_solve[1] + d_V
         solve = min_vel_solve
         while vel < max_shooter_velocity:
-            solve = fixed_velocity(x,y,vx,vy,vel,solve[4])
+            solve = fixed_velocity(x, y, vx, vy, vel, solve[4])
             if not solve[0]:
                 break
             velocities.append(vel)
             angles.append(np.rad2deg(solve[2]))
             vel += d_V
         if solve[0]:
-            solve = fixed_velocity(x,y,vx,vy,max_shooter_velocity,solve[4])
+            solve = fixed_velocity(x, y, vx, vy, max_shooter_velocity, solve[4])
             velocities.append(max_shooter_velocity)
             angles.append(np.rad2deg(solve[2]))
         plt.plot(velocities, angles)
